46206cfbcbd12236c4ab12910d8218a9474244d3
swankhede <31096252+swankhede@users.noreply.github.com>
2019-12-23 20:01:50 +0530

    Update p5.py

--- p5.py
+++ p5.py
@@ -4,7 +4,7 @@ import requests
 try:
 	socket.create_connection(("www.google.com",80))
 	print("u r connected")
-	city = input("enter city name ")
+	city = input("enter city name: ")
 	a1 = "http://api.openweathermap.org/data/2.5/weather?"
 
 	a2 = "q=" +city + "&units=metric"
