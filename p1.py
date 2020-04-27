7844942aae38939cd494a5e58d11a06dcee483b3
swankhede <31096252+swankhede@users.noreply.github.com>
2020-04-27 12:58:07 +0530

    Delete p1.py

--- p1.py
+++
@@ -1,8 +0,0 @@
-import socket
-import requests
-url = "http://mu.ac.in/portal/wp-content/uploads/2014/11/1T033121.pdf"
-res = requests.get(url)
-print(res)
-f = open("timetable.pdf","wb")
-f.write(res.content)
-f.close()
