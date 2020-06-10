24541252b04f8c50a6649329202932e7bb54fd73
swankhede <31096252+swankhede@users.noreply.github.com>
2020-06-10 15:27:50 +0530

    Add files via upload

---
+++ BounceBall.py
@@ -0,0 +1,107 @@
+import turtle
+import random
+import time
+
+frame = turtle.Screen()
+frame.title("Bounce Ball")
+frame.bgcolor("black")
+frame.setup(width=500,height=500)
+frame.tracer(0)
+
+paddle = turtle.Turtle()
+paddle.speed(1)
+paddle.shape('square')
+paddle.color('white')
+paddle.shapesize(stretch_wid=0.5,stretch_len=5)
+paddle.penup()
+paddle.goto(-200,-200)
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape('circle')
+ball.color('white')
+
+ball.penup()
+ball.goto(0,200)
+ball.dx=2
+ball.dy=-2
+
+
+def moveRight():
+    x=paddle.xcor()
+    x+=10   
+    paddle.setx(x)
+
+def moveLeft():
+    x=paddle.xcor()
+    x-=10
+    paddle.setx(x)
+
+
+frame.listen()
+frame.onkeypress(moveRight,'Right')
+frame.onkeypress(moveLeft,'Left')
+
+cor = random.randint(-280,280)
+ball.setx(cor)
+ball.sety(250)
+score=0
+while True:
+    frame.update()
+  
+    ball.sety(ball.ycor()+ball.dy)
+    ball.setx(ball.xcor()+ball.dx)
+    time.sleep(0.005)
+    
+    if ball.xcor()>250:
+        ball.dx *=-1
+    
+    if ball.xcor()<-250:
+        print(ball.xcor())
+        ball.dx*=-1
+    
+    if ball.ycor()<-250:
+        ball.dy*=-1
+        ball.sety(ball.ycor()+ball.dy)
+    
+    if ball.ycor()>250:
+        ball.dy*=-1
+        ball.sety(ball.ycor()+ball.dy)
+    
+       
+        
+        
+    
+    
+       
+
+    
+    
+
+    
+    """ if ball.ycor()<-250 or :
+        
+        cor = random.randint(-300,300)
+        ball.setx(cor)
+        ball.sety(300)"""
+      
+    #print(paddle.ycor(),paddle.xcor())
+    if (ball.dy<0 and ball.ycor()<=-185 and ball.ycor()>=-200) and(paddle.xcor()-10 <= ball.xcor() <= paddle.xcor()+10):
+        ball.dy*=-1
+        
+        score =score+1
+        print("score:",score)
+    
+        print(ball.ycor(),ball.xcor())
+        print(paddle.ycor(),paddle.xcor())
+   
+
+       
+        
+    
+
+        
+    
+    
+    
+    
